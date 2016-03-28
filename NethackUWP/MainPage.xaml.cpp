49f911d2a2960923ee090fe434d0f71b72de7d07
Scott Greenlay <sgreenlay@gmail.com>
2016-03-27 21:14:11 -0700

    Fix role randomization. Added notifications window. Use databinding for map display.

--- NethackUWP/MainPage.xaml.cpp
+++ NethackUWP/MainPage.xaml.cpp
@@ -41,14 +41,19 @@ using namespace Windows::UI::Xaml::Navigation;
 MainPage::MainPage()
 {
 	InitializeComponent();
+    if (g_mainpage != nullptr)
+        abort();
+    g_mainpage = this;
+    g_corewindow = Windows::UI::Core::CoreWindow::GetForCurrentThread();
+    Notifications = ref new Platform::Collections::Vector<Platform::String^>();
+    this->DataContext = this;
 }
-
 
 void NethackUWP::MainPage::button_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
 {
 	if (game_is_running == true) return;
 	game_is_running = true;
-	static thread nethack_thread([]()
+    static thread nethack_thread([]()
 	{
 		sys_early_init();
 		choose_windows("mswin");//dun worry
@@ -69,42 +74,53 @@ void NethackUWP::MainPage::button_Click(Platform::Object^ sender, Windows::UI::X
 	
 }
 
-extern deque<char> input_string;
-extern deque<char> output_string; //XXX
-extern mutex blocked_on_input;
-extern condition_variable input_string_cv;
-
 void NethackUWP::MainPage::Send_butt_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
 {
+    {
+        lock_guard<mutex> lock(blocked_on_input);
 
-		unique_lock<mutex> blocked_on_input_lock(blocked_on_input);
-
-		input_string.insert(input_string.end(),begin(InputBox->Text), end(InputBox->Text));
-		input_string.push_back('\n');
-		InputBox->Text = "";
-		input_string_cv.notify_one();
-		//XXX XXX HACK NOT SAFE DO NOT USE NSFW PLZ REMOVE
-		//THIS WILL BREAK.
-		wstring my_sad_performance;
-		while (!output_string.empty())
-		{
-			char wtf = output_string.at(0);
-			output_string.pop_front();
-			//my_sad_performance.append(wtf);
-			my_sad_performance.push_back(wtf);
-			
-			
-		}
-		
-
-		OutputBox->Text = ref new String(my_sad_performance.c_str());
-		//OutputBox->Text = my_sad_performance;
-
+        if (input_string.empty())
+            input_string_cv.notify_all();
+        input_string.insert(input_string.end(), begin(InputBox->Text), end(InputBox->Text));
+        input_string.push_back('\n');
+    }
+    InputBox->Text = "";
 }
 
-
-void NethackUWP::MainPage::recv_char_print(int c)
+int NativeMainPage::read_char()
 {
-	//OutputBox->Text += String c;
+    unique_lock<mutex> lock(g_mainpage->blocked_on_input);
+    while (g_mainpage->input_string.empty())
+    {
+        g_mainpage->input_string_cv.wait(lock);
+    }
+
+    int ret = g_mainpage->input_string.back();
+    g_mainpage->input_string.pop_back();
+
+    return ret;
 }
 
+using namespace Windows::UI::Core;
+
+void NativeMainPage::write_char(int ch)
+{
+    {
+        lock_guard<mutex> lock(g_mainpage->blocked_on_output);
+
+        g_mainpage->output_string.push_back(ch);
+    }
+    g_corewindow->Dispatcher->RunAsync(CoreDispatcherPriority::Low, ref new DispatchedHandler([]() {
+        g_mainpage->PropertyChanged(g_mainpage, ref new PropertyChangedEventArgs("OutStringBuf"));
+    }));
+}
+
+void NativeMainPage::write_notification(const char * str)
+{
+    std::wstring strbuf;
+    while (*str) { strbuf.push_back(*str); ++str; }
+    Platform::String^ pcstr = ref new Platform::String(strbuf.c_str());
+    g_corewindow->Dispatcher->RunAsync(CoreDispatcherPriority::Low, ref new DispatchedHandler([pcstr]() {
+        g_mainpage->Notifications->Append(pcstr);
+    }));
+}
--- NethackUWP/MainPage.xaml.h
+++ NethackUWP/MainPage.xaml.h
@@ -6,21 +6,54 @@
 #pragma once
 
 #include "MainPage.g.h"
+#include <deque>
+#include <mutex>
+#include <condition_variable>
+#include <vector>
+
+struct NativeMainPage {
+    static int read_char();
+    static void write_char(int);
+ 
+    static void write_notification(const char*);
+};
 
 namespace NethackUWP
 {
 	/// <summary>
 	/// An empty page that can be used on its own or navigated to within a Frame.
 	/// </summary>
-	public ref class MainPage sealed
+    [Windows::UI::Xaml::Data::Bindable]
+	public ref class MainPage sealed : public Windows::UI::Xaml::Data::INotifyPropertyChanged
 	{
 	public:
 		MainPage();
-		void recv_char_print(int c);
+
+        property Platform::String^ OutStringBuf {
+            Platform::String^ get() {
+                std::lock_guard<std::mutex> lock(blocked_on_output);
+                return ref new Platform::String(output_string.c_str());
+            }
+        }
+
+        property Windows::Foundation::Collections::IVector<Platform::String^>^ Notifications;
+
+        virtual event Windows::UI::Xaml::Data::PropertyChangedEventHandler ^ PropertyChanged;
 
 	private:
 		void button_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
 		void Send_butt_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
 		void InputBox_TextChanged(Platform::Object^ sender, Windows::UI::Xaml::Controls::TextChangedEventArgs^ e);
+
+        std::deque<char> input_string;
+        std::mutex blocked_on_input;
+        std::condition_variable input_string_cv;
+
+        std::mutex blocked_on_output;
+        std::wstring output_string;
+
+        friend struct ::NativeMainPage;
 	};
+    static MainPage^ g_mainpage;
+    static Platform::Agile<Windows::UI::Core::CoreWindow> g_corewindow;
 }
