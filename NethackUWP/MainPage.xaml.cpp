5eaa81584c462aab190d92c48f0538b2736ae85e
Robert Schumacher <ras0219@outlook.com>
2016-05-22 23:31:59 -0700

    CMake?

--- NethackUWP/MainPage.xaml.cpp
+++ NethackUWP/MainPage.xaml.cpp
@@ -5,25 +5,8 @@
 
 #include "pch.h"
 #include "MainPage.xaml.h"
-#include <deque>
-#include <thread>
-#include <mutex>
-
-bool game_is_running = false;
-bool waiting_for_input = false;
-#define boolean boolean2
-#define terminate terminate2
-
-extern "C" {
-#include "hack.h"
-#include "dlb.h"
-}
-#undef terminate
-#undef boolean
 
 using namespace NethackUWP;
-using namespace std;
-
 
 using namespace Platform;
 using namespace Windows::Foundation;
@@ -40,102 +23,5 @@ using namespace Windows::UI::Xaml::Navigation;
 
 MainPage::MainPage()
 {
-	InitializeComponent();
-    if (g_mainpage != nullptr)
-        abort();
-    g_mainpage = this;
-    g_corewindow = Windows::UI::Core::CoreWindow::GetForCurrentThread();
-    Notifications = ref new Platform::Collections::Vector<Platform::String^>();
-    this->DataContext = this;
-
-    output_string = std::wstring(NativeMainPage::max_width_offset * NativeMainPage::max_height, L'C');
-    for (int x = 1; x <= NativeMainPage::max_height; ++x)
-    {
-        output_string[x * NativeMainPage::max_width_offset - 1] = '\n';
-    }
-}
-
-void NethackUWP::MainPage::button_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
-{
-	if (game_is_running == true) return;
-	game_is_running = true;
-    static thread nethack_thread([]()
-	{
-		sys_early_init();
-		choose_windows("mswin");//dun worry
-							  //	tty_procs (NULL, NULL); //dun worry
-		initoptions(); //nuh nuh nuh
-
-		dlb_init();
-		init_nhwindows(0, 0);
-		vision_init();
-		display_gamewindows();//dunno
-		newgame();
-		
-		//resuming = pcmain(argc, argv);
-
-		moveloop(0);
-		//trololololololololololololololololololo
-	});
-	
-}
-
-void NethackUWP::MainPage::Send_butt_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
-{
-    {
-        lock_guard<mutex> lock(blocked_on_input);
-
-        if (input_string.empty())
-            input_string_cv.notify_all();
-        input_string.insert(input_string.end(), begin(InputBox->Text), end(InputBox->Text));
-    }
-    InputBox->Text = "";
-}
-
-int NativeMainPage::read_char()
-{
-    unique_lock<mutex> lock(g_mainpage->blocked_on_input);
-    while (g_mainpage->input_string.empty())
-    {
-        g_mainpage->input_string_cv.wait(lock);
-    }
-
-    int ret = g_mainpage->input_string.back();
-    g_mainpage->input_string.pop_back();
-
-    return ret;
-}
-
-using namespace Windows::UI::Core;
-
-void NativeMainPage::write_char(int x, int y, char ch)
-{
-    {
-        lock_guard<mutex> lock(g_mainpage->blocked_on_output);
-        if (x >= max_width) abort();
-        if (y >= max_height) abort();
-        g_mainpage->output_string[max_width_offset * y + x] = ch;
-    }
-    g_corewindow->Dispatcher->RunAsync(CoreDispatcherPriority::Low, ref new DispatchedHandler([]() {
-        g_mainpage->PropertyChanged(g_mainpage, ref new PropertyChangedEventArgs("OutStringBuf"));
-    }));
-}
-
-void NativeMainPage::write_notification(const char * str)
-{
-    if (str == 0 || *str == 0)
-        return;
-
-    std::wstring strbuf;
-    while (*str) { strbuf.push_back(*str); ++str; }
-    Platform::String^ pcstr = ref new Platform::String(strbuf.c_str());
-    g_corewindow->Dispatcher->RunAsync(CoreDispatcherPriority::Low, ref new DispatchedHandler([pcstr]() {
-        g_mainpage->Notifications->Append(pcstr);
-    }));
-}
-
-
-void NethackUWP::MainPage::OutputBox_TextChanged(Platform::Object^ sender, Windows::UI::Xaml::Controls::TextChangedEventArgs^ e)
-{
-
+    InitializeComponent();
 }
--- NethackUWP/MainPage.xaml.h
+++ NethackUWP/MainPage.xaml.h
@@ -6,58 +6,16 @@
 #pragma once
 
 #include "MainPage.g.h"
-#include <deque>
-#include <mutex>
-#include <condition_variable>
-#include <vector>
-
-struct NativeMainPage {
-    static constexpr int max_width = 100;
-    static constexpr int max_width_offset = max_width + 1;
-    static constexpr int max_height = 80;
-    static int read_char();
-    static void write_char(int x, int y, char ch);
- 
-    static void write_notification(const char*);
-};
 
 namespace NethackUWP
 {
-	/// <summary>
-	/// An empty page that can be used on its own or navigated to within a Frame.
-	/// </summary>
-    [Windows::UI::Xaml::Data::Bindable]
-	public ref class MainPage sealed : public Windows::UI::Xaml::Data::INotifyPropertyChanged
-	{
-	public:
-		MainPage();
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public ref class MainPage sealed
+    {
+    public:
+        MainPage();
 
-        property Platform::String^ OutStringBuf {
-            Platform::String^ get() {
-                std::lock_guard<std::mutex> lock(blocked_on_output);
-                return ref new Platform::String(output_string.c_str());
-            }
-        }
-
-        property Windows::Foundation::Collections::IVector<Platform::String^>^ Notifications;
-
-        virtual event Windows::UI::Xaml::Data::PropertyChangedEventHandler ^ PropertyChanged;
-
-	private:
-		void button_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
-		void Send_butt_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
-		void InputBox_TextChanged(Platform::Object^ sender, Windows::UI::Xaml::Controls::TextChangedEventArgs^ e);
-
-        std::deque<wchar_t> input_string;
-        std::mutex blocked_on_input;
-        std::condition_variable input_string_cv;
-
-        std::mutex blocked_on_output;
-        std::wstring output_string;
-
-        friend struct ::NativeMainPage;
-        void OutputBox_TextChanged(Platform::Object^ sender, Windows::UI::Xaml::Controls::TextChangedEventArgs^ e);
     };
-    static MainPage^ g_mainpage;
-    static Platform::Agile<Windows::UI::Core::CoreWindow> g_corewindow;
 }
